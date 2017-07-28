6f88fd67775696df23a0c63ddde6300ab43f53ab
Matthew Huebert <no-email@gmail.com>
2017-07-28 14:44:37 +0200

    Fix middleware

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject figwheel-pushstate-server "0.1.1"
+(defproject figwheel-pushstate-server "0.1.2"
   :description "Figwheel ring handler to support pushstate routes"
   :url "https://github.com/mhuebert/figwheel-pushstate-server"
   :license "MIT"
   :dependencies [[ring "1.4.0"]
                  [ring/ring-defaults "0.2.0"]
+                 [ring/ring-core "1.6.0"]
                  [compojure "1.5.0"]]
   :deploy-via :clojars
   :src-paths ["src"])
--- src/figwheel_server/core.clj
+++ src/figwheel_server/core.clj
@@ -2,7 +2,11 @@
     (:require
       [clojure.java.io :as io]
       [compojure.core :as c]
-      [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+      [ring.middleware.defaults :as defaults]
+      [ring.middleware.resource :refer [wrap-resource]]
+      [ring.middleware.file :refer [wrap-file]]
+      [ring.middleware.content-type :refer [wrap-content-type]]
+      [ring.middleware.default-charset :refer [wrap-default-charset]]))
 
 (c/defroutes routes
              (c/ANY "*" _
@@ -10,5 +14,28 @@
                      :headers {"Content-Type"    "text/html; charset=utf-8"}
                      :body    (io/input-stream (io/resource "public/index.html"))}))
 
+;; from ring.middleware.defaults
+(defn- wrap [handler middleware options]
+  (if (true? options)
+    (middleware handler)
+    (if options
+      (middleware handler options)
+      handler)))
+
+(defn wrap-defaults
+  "Wraps a handler in default Ring middleware, as specified by the supplied
+  configuration map.
+
+  See: api-defaults
+       site-defaults
+       secure-api-defaults
+       secure-site-defaults"
+  [handler config]
+  (-> handler
+      (wrap wrap-resource         (get-in config [:static :resources] false))
+      (wrap wrap-file             (get-in config [:static :files] false))
+      (wrap wrap-content-type     (get-in config [:responses :content-types] false))
+      (wrap wrap-default-charset  (get-in config [:responses :default-charset] false))))
+
 (def handler
-  (wrap-defaults routes (update site-defaults :security dissoc :frame-options)))
+  (wrap-defaults routes defaults/site-defaults))
